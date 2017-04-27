49c87e5ff9209bf048d2ce439b503cc3e43545e5
BlackScorp <witalimik@web.de>
2017-04-27 22:07:30 +0200

    created rect class

---
+++ source/Shapes/Rect.cpp
@@ -0,0 +1,56 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 BlackScorp.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* 
+ * File:   Rect.cpp
+ * Author: BlackScorp
+ * 
+ * Created on 27. April 2017, 20:34
+ */
+
+#include <iostream>
+
+#include "Rect.h"
+
+Rect::Rect(int width, int height) {
+    this->width = width;
+    this->height = height;
+    this->x = 0;
+    this->y = 0;
+}
+
+Rect::~Rect() {
+}
+
+void Rect::onDraw(SDL_Renderer* renderer,float interpolation) {
+    std::cout << "Draw rect";
+  SDL_Rect r;
+    r.x = x;
+    r.y = y;
+    r.w = width;
+    r.h = height;
+    SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+    SDL_RenderFillRect(renderer, &r);
+    
+}
